bbcb571a7e591ed52bb598451b647777c4e2ac49
dell <dell>
2021-01-18 11:49:12 +0800

    初始化

---
+++ springboot-amqp/src/test/java/cn/lihuan/com/AmqpApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lihuan.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmqpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
