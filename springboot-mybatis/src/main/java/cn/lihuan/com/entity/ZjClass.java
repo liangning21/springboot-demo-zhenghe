2c8bb7068beaceff89404a231a972cb9b3781f38
liangning21 <dell>
2021-02-08 11:51:54 +0800

    mybatis整合

---
+++ springboot-mybatis/src/main/java/cn/lihuan/com/entity/ZjClass.java
@@ -0,0 +1,18 @@
+package cn.lihuan.com.entity;
+
+import lombok.Data;
+
+/**
+ * @Classname lihuan
+ * @Description TODO
+ * @Date 2021/2/8 10:59
+ * @Created by Dell
+ */
+@Data
+public class ZjClass {
+
+    private String zcId;
+    private String zcName;
+    private String zcCreatetime;
+    private String zcDel;
+}
